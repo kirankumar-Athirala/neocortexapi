53c7f1f5ae19f7f1bc35fe27c2ea34356d0b3bf6
kirankumar athirala <46919805+kirankumar-Athirala@users.noreply.github.com>
2022-02-10 21:54:13 +0100

    Analysing UpdateBoostFactors method using maxBoost,DutyCyclePeriod and IsBumpUpWeakColumnsDisabled parameters.

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -22,8 +22,8 @@ namespace NeoCortexApiSample
 
             // Used as a boosting parameters
             // that ensure homeostatic plasticity effect.
-            double minOctOverlapCycles = 10;
-            double maxBoost = 5;
+            double minOctOverlapCycles = 1;
+            double maxBoost = 1;
 
             // We will use 200 bits to represent an input vector (pattern).
             int inputBits = 200;
@@ -37,7 +37,8 @@ namespace NeoCortexApiSample
             {
                 CellsPerColumn = 10,
                 MaxBoost = maxBoost,
-                DutyCyclePeriod = 100,
+                DutyCyclePeriod = 150000,
+                IsBumpUpWeakColumnsDisabled =true,
                 MinPctOverlapDutyCycles = minOctOverlapCycles,
 
                 GlobalInhibition = true,
