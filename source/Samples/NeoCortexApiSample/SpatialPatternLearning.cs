85202b10f1c8458c89c8987ac1b4051d857a1f41
kirankumar athirala <46919805+kirankumar-Athirala@users.noreply.github.com>
2022-02-13 19:45:55 +0100

    Analysis of UpdateBoostFactors method,

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -23,7 +23,7 @@ namespace NeoCortexApiSample
             // Used as a boosting parameters
             // that ensure homeostatic plasticity effect.
             double minOctOverlapCycles = 1;
-            double maxBoost = 1;
+            double maxBoost = 10;
 
             // We will use 200 bits to represent an input vector (pattern).
             int inputBits = 200;
